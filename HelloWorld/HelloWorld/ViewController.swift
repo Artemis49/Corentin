ef8c5f151ab5269a844002a746a6ab6548f5d9d3
Corentin Chateau <cchateau@afoneinfrastructure.com>
2019-10-23 15:23:19 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Corentin Chateau on 23/10/2019.
+//  Copyright © 2019 Corentin Chateau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
