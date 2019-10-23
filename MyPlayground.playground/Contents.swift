1044d2516be58e418bd4830001975d67c20369d1
Corentin Chateau <cchateau@afoneinfrastructure.com>
2019-10-23 17:39:39 +0200

    playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// This is comment
+var str = "Je suis une string"
+
+func sayHello(truc:String) {
+    if truc != "" {
+        print(truc+" modifiée !")
+    }
+}
+
+sayHello(truc: str)
